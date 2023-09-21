91199882c475ee5eea1dd38d52b812a9f3d2f7a0
njpd57 <njpd57@gmail.com>
2023-09-20 21:20:23 -0300

    V2.0, con IA mejorada, sugerencias, menu, dificultad, etc

--- banuversi.py
+++ banuversi.py
@@ -1,19 +1,28 @@
 '''
-Diego Campillay Manriquez
 Nicolas Palacios Diaz
 Ingenieria Civil Informatica
 Universidad Andres Bello
 CINF 103
+TAREA N°1 FUNDAMENTOS IA
+202320
 '''
-
-from ast import While
 import numpy as np
 import copy
 import sys
 from time import time
 import pygame
+import pygame_menu
 from pygame.locals import *
+from pygamepopup.menu_manager import MenuManager
+from pygamepopup.components import Button, InfoBox
+import pygamepopup
+
+
 pygame.init()
+pygamepopup.init()
+
+from myconfig import minimaxConf
+
 
 
 #Color de fondo
@@ -24,24 +33,26 @@ FPS = 60 #Frames por segundo
 fpsClock = pygame.time.Clock() #Reloj
 WINDOW_WIDTH = 800 #Largo pantalla
 WINDOW_HEIGHT = 850 #Alto de pantalla
-WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) #Setear la resolucion
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) #Establecer la resolucion
+menu_manager = MenuManager(WINDOW)
 #Nombre aplicacion
-pygame.display.set_caption('BanuVersi')
+pygame.display.set_caption('BanuVersi V2.0')
 #Cargar imagenes
-FONDO = pygame.image.load('Assets/Fondo.png')
+FONDO_8 = pygame.image.load('Assets/Fondo_8.png')
+FONDO_6 = pygame.image.load('Assets/Fondo_6.png')
 PIEZABLANCA = pygame.image.load('Assets/Blanco.png')
 PIEZANEGRA = pygame.image.load('Assets/Negro.png')
 JUGADA = pygame.image.load('Assets/piezasJugables.png')
+SUGERENCIA = pygame.image.load('Assets/piezaSugerencia.png')
 
 #Cargar Texto
 FONT = pygame.font.SysFont('Comic Sans MS', 30)
-TEXTO_JUGADOR = FONT.render('Turno Jugador',True,(255, 255, 255))
+TEXTO_JUGADOR = FONT.render('Turno Jugador, presione s para sugerencia',True,(255, 255, 255))
 TEXTO_WHITE = FONT.render('Turno Maquina, Pensando...',True,(255, 255, 255))
 GANAR = FONT.render('Ganaste viva Banu por siempre!',True,(255, 255, 255))
 PERDER = FONT.render('Perdiste... Apreta R para reiniciar',True,(255, 255, 255))
 
-#Columnas
-CANTIDAD_COLUMNAS = 8
+
 # Constantes para saber que hay en tablero y para comparar durante el juego (WHITE = IA  BLACK = Jugador)
 WHITE = 1
 VACIO = 0
@@ -49,7 +60,9 @@ BLACK = -1
 
 EMPATE = 2
 GANADOR_VAL = 100
-DEMO_SEARCH_DEPTH = 5 #Profundidad de busqueda de prueba
+
+#Configuración
+banuconfig = minimaxConf
 
 # Direcciones posibles donde insertar siguientes fichas
 DIRECCIONES = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
@@ -71,19 +84,18 @@ def encontrar_ganador(tablero):
 # generar_mov_legal retorna los movimientos posibles
 
 def generar_mov_legal(tablero, turno_white):
+
     mov_legales = []
-    for r in range(CANTIDAD_COLUMNAS):
-        for c in range(CANTIDAD_COLUMNAS):
+    
+    for r in range(banuconfig.CANTIDAD_COLUMNAS):
+        for c in range(banuconfig.CANTIDAD_COLUMNAS):
             if tablero[r][c] != VACIO:
                 continue
             if puede_capturar(tablero, r, c, turno_white):
                 mov_legales.append((r, c))
     return mov_legales
 
-
-
 # Verifica capturas posibles en las 8 direcciones
-
 
 def puede_capturar(tablero, r, c, turno_white):
     for r_delta, c_delta in DIRECCIONES:
@@ -92,12 +104,10 @@ def puede_capturar(tablero, r, c, turno_white):
     return False
 
 # Verifica capturas cuando la columna y fila estan modificadas con row como row_delta, col como col_delta
-
-
 def capturas_en_dir(tablero, row, row_delta, col, col_delta, turno_white):
-    if (row+row_delta < 0) or (row+row_delta >= CANTIDAD_COLUMNAS):
+    if (row+row_delta < 0) or (row+row_delta >= banuconfig.CANTIDAD_COLUMNAS):
         return False
-    if (col+col_delta < 0) or (col+col_delta >= CANTIDAD_COLUMNAS):
+    if (col+col_delta < 0) or (col+col_delta >= banuconfig.CANTIDAD_COLUMNAS):
         return False
 
     # No puede capturar si esta el color enemigo en esa casilla
@@ -111,7 +121,7 @@ def capturas_en_dir(tablero, row, row_delta, col, col_delta, turno_white):
     color_amistoso = WHITE if turno_white else BLACK
     scan_row = row + 2*row_delta  # fila del primer escaneo de posicion
     scan_col = col + 2*col_delta  # columna del primer escaneo de posicion
-    while (scan_row >= 0) and (scan_row < CANTIDAD_COLUMNAS) and (scan_col >= 0) and (scan_col < CANTIDAD_COLUMNAS):
+    while (scan_row >= 0) and (scan_row < banuconfig.CANTIDAD_COLUMNAS) and (scan_col >= 0) and (scan_col < banuconfig.CANTIDAD_COLUMNAS):
         if tablero[scan_row][scan_col] == VACIO:
             return False
         if tablero[scan_row][scan_col] == color_amistoso:
@@ -121,8 +131,6 @@ def capturas_en_dir(tablero, row, row_delta, col, col_delta, turno_white):
     return False
 
 # Cambia el tablero para representar las piezas capturadas por el movimiento (row,col)
-
-
 def captura(tablero, row, col, turno_white):
     # Verifica en cada direccion si hay una ficha que se pudo dar vuelta, si es asi empieza a voltearlas
     color_enemigo = BLACK if turno_white else WHITE
@@ -214,34 +222,34 @@ def minimax_valor(tablero, turno_white, search_depth, alpha, beta):
                     break
         return minScore
 
-
 def imprimir_tablero(tablero):
     printable = {
         -1: "1",
         0: "-",
         1: "2"
     }
-    for r in range(CANTIDAD_COLUMNAS):
+    for r in range(banuconfig.CANTIDAD_COLUMNAS):
         line = ""
-        for c in range(CANTIDAD_COLUMNAS):
+        for c in range(banuconfig.CANTIDAD_COLUMNAS):
             line += printable[tablero[r][c]]
         print(line)
 
 
 
 def inicializar_tablero():
-    tablero = np.zeros((CANTIDAD_COLUMNAS, CANTIDAD_COLUMNAS))
-    tablero[3][3] = WHITE
-    tablero[3][4] = BLACK
-    tablero[4][3] = BLACK
-    tablero[4][4] = WHITE
+    tablero = np.zeros((banuconfig.CANTIDAD_COLUMNAS, banuconfig.CANTIDAD_COLUMNAS))
+    offset = 1 if banuconfig.CANTIDAD_COLUMNAS == 6 else 0
+    tablero[3-offset][3-offset] = WHITE
+    tablero[3-offset][4-offset] = BLACK
+    tablero[4-offset][3-offset] = BLACK
+    tablero[4-offset][4-offset] = WHITE
     return tablero
 
-
-def renderizarPiezas(tablero, render,legalTurns):
-    y = 0
+def renderizarPiezas(tablero, render,legalTurns,sugerencia=None):
+    offset = 1 if banuconfig.CANTIDAD_COLUMNAS == 6 else 0
+    y = 0 + offset
     for i in tablero:
-        x = 0
+        x = 0 + offset
         for j in i:
             #print([j,x,y])
             if j == 1.0:
@@ -253,31 +261,49 @@ def renderizarPiezas(tablero, render,legalTurns):
             x += 1
         y += 1
     for i in legalTurns:
-        render.blit(JUGADA,(i[1]*100,i[0]*100))
+        
+        if sugerencia != None:
+            print(sugerencia)
+            if i in legalTurns and i == sugerencia:
+                render.blit(SUGERENCIA,( (i[1] + offset )*100, (i[0] + offset)*100))
+            elif i in legalTurns:
+                render.blit(JUGADA,( (i[1] + offset )*100, (i[0] + offset)*100))
+        else:
+            render.blit(JUGADA,( (i[1] + offset )*100, (i[0] + offset)*100))
         #print(i[0]*100)
+
+
 
 
 def click(posicion_mouse,posibles_jugadas):
     #Transformar entrada de usuario a coordenadas de tablero
-    x = posicion_mouse[0]//100 
-    y = posicion_mouse[1]//100
-    #print([x,y])
+    offset = 1 if banuconfig.CANTIDAD_COLUMNAS == 6 else 0
+    x = posicion_mouse[0] //100
+    y = posicion_mouse[1] //100
 
-    if (y,x) in posibles_jugadas:
-        return (y,x)
+    if (y-offset,x-offset) in posibles_jugadas:
+        return (y-offset,x-offset)
     return 0
 
 
-def main():
+def jugar():
     loop_juego = True
-    TURNO = WHITE
+    TURNO = BLACK
     tablero = inicializar_tablero()
+    print(banuconfig.CANTIDAD_COLUMNAS)
     enJuego = VACIO
+    sugerencias = None
+
+    if(banuconfig.CANTIDAD_COLUMNAS == 8):
+        FONDO = FONDO_8
+    else:
+        FONDO = FONDO_6
+
     while loop_juego:
-        
         WINDOW.fill(COLOR_FONDO)
+        
         WINDOW.blit(FONDO, (0, 0))
-        renderizarPiezas(tablero,WINDOW,generar_mov_legal(tablero,False))
+        renderizarPiezas(tablero,WINDOW,generar_mov_legal(tablero,False),sugerencias)
 
         if TURNO != WHITE and enJuego == VACIO:
             WINDOW.blit(TEXTO_JUGADOR, (400-30*13,800))
@@ -289,6 +315,7 @@ def main():
         pygame.display.update()
         fpsClock.tick(FPS)
         enJuego = ver_game_over(tablero)
+
 
         #Si el juego aun no termina
         if  enJuego == VACIO:
@@ -304,15 +331,20 @@ def main():
                     best_mov = None
                     for m in posibles_movimientos:
                         new_tablero = play_mov(tablero, m, True)
+                        tiempo_inicial = time()
                         mov_val = minimax_valor(
-                        new_tablero, True, DEMO_SEARCH_DEPTH, float("-inf"), float("inf"))
+                        new_tablero, True, banuconfig.SEARCH_DEPTH, float("-inf"), float("inf"))
+
+                        tiempo_final = time()
+
+                        print(tiempo_final-tiempo_inicial)
                         if mov_val > best_val:
                             best_mov = m
                             best_val = mov_val
                     tablero = play_mov(tablero, best_mov, True)
                     imprimir_tablero(tablero)
                 else:
-                    print("Computador no EMPATEne opciones; saltar turno...")
+                    print("Computador no tiene opciones; saltar turno...")
                     
                 TURNO = BLACK
                 
@@ -329,12 +361,31 @@ def main():
                         elif event.type == QUIT:
                             pygame.quit()
                             sys.exit()
+                        elif event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_s:
+                                    best_val = float("-inf")
+                                    best_mov = None
+                                    for m in posibles_movimientos:
+                                        new_tablero = play_mov(tablero, m, True)
+                                        tiempo_inicial = time()
+                                        mov_val = minimax_valor(
+                                        new_tablero, True, banuconfig.SEARCH_DEPTH, float("-inf"), float("inf"))
+
+                                        tiempo_final = time()
+
+                                        print(tiempo_final-tiempo_inicial)
+                                        if mov_val > best_val:
+                                            best_mov = m
+                                            best_val = mov_val
+                                    sugerencias = best_mov
+                                    
+                                    
                     if(player_mov != 0):
                         tablero = play_mov(tablero, player_mov, False)
                         imprimir_tablero(tablero)
                         TURNO = WHITE
                 else:
-                    print("Jugador no EMPATEne opciones; saltar turno...")  
+                    print("Jugador no tiene opciones; saltar turno...")  
                     TURNO = WHITE
         #Si termino, opcion de apretar R para reiniciar
         else:
@@ -343,6 +394,31 @@ def main():
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_r:
                         tablero = inicializar_tablero()
+
+
+
+def establecer_dificultad(_, dificultad):
+    banuconfig.SEARCH_DEPTH = dificultad
+    print(banuconfig.SEARCH_DEPTH)
+
+def establecer_tamanno(_, tamanno):
+    banuconfig.CANTIDAD_COLUMNAS = tamanno
+    print(banuconfig.CANTIDAD_COLUMNAS  )
+    
+
+
+def main():
+
+    
+    #CONFIGURACION
+    menu = pygame_menu.Menu('Banuversi V2.0', 400, 300,
+                       theme=pygame_menu.themes.THEME_BLUE)
+    menu.add.selector('Dificultad :', [('Facil', 2), ('Intermedio', 5),('Imposible',8)], onchange=establecer_dificultad)
+    menu.add.selector('Tamaño de tablero :', [('6x6', 6), ('8x8', 8)], onchange=establecer_tamanno)
+    menu.add.button('Jugar', jugar)
+    menu.add.button('Salir', pygame_menu.events.EXIT)
+    menu.mainloop(WINDOW)
+
             
 
 
---
+++ myconfig.py
@@ -0,0 +1,8 @@
+
+class minimaxConf():
+    SEARCH_DEPTH = 2 #2 Facil, 5 Intermedio, 8 imposible
+    #Columnas
+    CANTIDAD_COLUMNAS = 6
+    
+
+        
